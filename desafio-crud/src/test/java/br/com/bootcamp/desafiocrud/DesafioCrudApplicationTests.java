2d5eb8dfdd0ee0f3cf682d78c283c78ae6c80c8b
tobiasb14 <tobiasb29@gmail.com>
2021-09-26 00:54:19 -0300

    first commit

---
+++ desafio-crud/src/test/java/br/com/bootcamp/desafiocrud/DesafioCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp.desafiocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
